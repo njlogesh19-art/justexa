75d71545e6879277cdb41aa8a9d78d0b1ff7b24f
njlogesh19-art <njlogesh19@gmail..com>
2026-02-26 08:26:41 +0530

    fix: use env var for Google Client ID

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <GoogleOAuthProvider clientId="834570037310-v7mkgea50s4i5jlg8bhi2bt97utgkpmc.apps.googleusercontent.com">
+        <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID || "834570037310-v7mkgea50s4i5jlg8bhi2bt97utgkpmc.apps.googleusercontent.com"}>
             <App />
         </GoogleOAuthProvider>
     </React.StrictMode>
